f7ba270b510f94398e3d269033186f6343ed8e4a
syeet <samclarpan@gmail.com>
2020-07-14 18:10:15 +0800

    change index.js

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,2 +1,48 @@
-// first JavaScript code lol
+// first JavaScript code lol;
 console.log('Hello World hehe');
+
+//variables:
+let name = 'lolerz';
+console.log(name);
+
+//variables cannot be a reserved keyword 
+//variables should be meaningful 
+//variables canNOT start w a number 
+//cannot contain spaces or - 
+
+
+//constants:
+
+const intrestRate = 0.3;
+//interestRate = 1;
+console.log(intrestRate);
+
+//if theres no need to reasign then use "const" 
+//if there is a need to reasign then use "let"
+
+//primitive types:
+
+let nextName ='sam'; //string literal
+let age = 15; //number literal
+let isApproved = false; //Boolean literal (can only be true or false)
+let firstName; 
+let lastName = null; //when you want to clear the value of a variable
+
+console.log(age);
+console.log(nextName);
+console.log(firstName);
+
+//objects:
+let person = {
+    name: 'kou ',
+    age: '17'
+};
+//Dot notation
+person.name = 'Dazai'; 
+console.log(person.name);
+
+//Bracket notation
+person['name'] = 'sammy dong dong';
+console.log(person.name);
+ 
+
